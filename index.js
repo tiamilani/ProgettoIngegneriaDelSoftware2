456ee0f00bd40c513fc9ca6459a9f6fb63650184
tiamilani <mattia.milani10@gmail.com>
2017-11-03 16:19:46 +0100

    # This is a combination of 2 commits. # This is the 1st commit message:
    Cercando di terminare il rebase
    
    # This is the commit message #2:
    
    .

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 const bot = require('./TelegramBot/bot');
+
+//Libraries
+var http = require('http');
+
+//create a server
+var server = http.createServer(
+    function (req, res) {
+        //HTML head (type of the page)
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+
+        //HTML content
+        res.end('Hello World');
+    }
+);
+
+//listen in a specific port
+server.listen(process.env.PORT || 80);
+
+//check status
+console.log('Server listening on port '+process.env.PORT);
 /*
 const express = require('express');
 const bodyParser = require('body-parser');
