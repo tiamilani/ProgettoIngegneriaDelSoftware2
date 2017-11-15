61097b41dbf353fe4a930c301bb03b39ecd60ad9
DAS-96 <damiano.sartori@studenti.unitn.it>
2017-11-15 09:01:03 +0100

    resolve small errors

--- api/controllers/places.js
+++ api/controllers/places.js
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-
-function placesNearby (resp, map, city, range, type, nome) {
-	map.placesNearby(
-		{
-			location: city,
-			radius: range,
-			language: "it",
-			type: type,
-			name: nome
-		},
-		function(err, response){
-			if (!err) {
-				//console.log(response.json);
-				var json = JSON.stringify({
-					response: response.json.results
-				})
-				resp.end(json);
-			}
-		}
-	);
-=======
 var NodeGeocoder = require('node-geocoder');
 
 function placesNearby (resp, map, city, range, type, nome, latlngKnown, geocoder) {
@@ -69,7 +47,6 @@ function placesNearby (resp, map, city, range, type, nome, latlngKnown, geocoder
 			);
 		});
 	}
->>>>>>> develop
 }
 
 exports.luoghiUtili = function(req, resp) {
@@ -80,14 +57,6 @@ exports.luoghiUtili = function(req, resp) {
 
 	var name = req.query.name;
 	var place = req.query.place;
-
-<<<<<<< HEAD
-	var type
-	var citta;
-
-	switch (place) {
-		case "trento":
-=======
 	var type;
 	var citta;
 
@@ -111,38 +80,20 @@ exports.luoghiUtili = function(req, resp) {
 	switch (place) {
 		case "trento":
 			latlngKnown = 0;
->>>>>>> develop
 			citta = {lat: 46.0702531, lng: 11.1216386};
 			console.log("Citta trento");
 			break;
 		case "mesiano":
-<<<<<<< HEAD
-=======
 			latlngKnown = 0;
->>>>>>> develop
 			citta = {lat: 46.0659393, lng: 11.1395838};
 			console.log("Citta mesiano");
 			break;
 		case "povo":
-<<<<<<< HEAD
-=======
 			latlngKnown = 0;
->>>>>>> develop
 			citta = {lat: 46.066294, lng: 11.153842};
 			console.log("Citta povo");
 			break;
 		case "oltrecastello":
-<<<<<<< HEAD
-			citta = {lat: 46.0659393, lng: 11.1395838};
-			console.log("Citta oltrecastello");
-			break;
-		case "specific":
-			citta={lat: req.query.lat, lng: req.query.lng};
-			console.log("Posizione specifica");
-			break;
-		default:
-			citta = {lat: 46.0702531, lng: 11.1216386};
-=======
 			latlngKnown = 0;
 			citta = {lat: 46.0659393, lng: 11.1395838};
 			console.log("Citta oltrecastello");
@@ -150,59 +101,28 @@ exports.luoghiUtili = function(req, resp) {
 		default:
 			latlngKnown = 1;
 			citta=place;
->>>>>>> develop
 			console.log("Citta default");
 			break;
 	}
 
-<<<<<<< HEAD
-	var map = require('@google/maps').createClient({
-		key: 'AIzaSyA_rBZuYeP8ONgMXRnIOpO0t0XWtod08lU'
-	});
-
-=======
->>>>>>> develop
 	var json;
 
 	switch (name) {
 		case "biblioteca":
 			console.log("Name biblioteca");
 			type="library";
-
-<<<<<<< HEAD
-			placesNearby(resp,map,citta,750,type,name);
-=======
 			placesNearby(resp,map,citta,1000,type,name,latlngKnown,geocoder);
->>>>>>> develop
-
 			break;
 		case "copisteria":
 			console.log("Name copisteria");
 			type="store";
-
-<<<<<<< HEAD
-			placesNearby(resp,map,citta,750,type,name);
-=======
 			placesNearby(resp,map,citta,1000,type,name,latlngKnown,geocoder);
->>>>>>> develop
-
 			break;
 		case "mensa":
 			console.log("Name mensa");
 			type="restaurant";
 			if(place == "trento"){
 				name="mensa opera universitaria";
-<<<<<<< HEAD
-				placesNearby(resp,map,citta,750,type,name);
-			}
-			else if(place == "mesiano" || place=="povo"){
-				name="mensa opera universitaria";
-				placesNearby(resp,map,citta,300,"",name);
-			}
-			else{
-				name = "mensa";
-				placesNearby(resp,map,citta,750,type,name);
-=======
 				placesNearby(resp,map,citta,750,type,name,latlngKnown,geocoder);
 			}
 			else if(place == "mesiano" || place=="povo"){
@@ -212,7 +132,6 @@ exports.luoghiUtili = function(req, resp) {
 			else{
 				name = "mensa";
 				placesNearby(resp,map,citta,750,type,name,latlngKnown,geocoder);
->>>>>>> develop
 			}
 
 			break;
@@ -222,36 +141,18 @@ exports.luoghiUtili = function(req, resp) {
 
 			if(place == "povo" || place == "mesiano"){
 				name="dipartimento";
-
-<<<<<<< HEAD
-				placesNearby(resp,map,citta,400,"",name);
-			}
-			else{
-				name="Univeristà";
-				placesNearby(resp,map,citta,750,type,name);
-=======
 				placesNearby(resp,map,citta,400,"",name,latlngKnown,geocoder);
 			}
 			else{
 				name="Univeristà";
 				placesNearby(resp,map,citta,1000,type,name,latlngKnown,geocoder);
->>>>>>> develop
 			}
 
 			break;
 		default:
 
 			console.log("Name default");
-<<<<<<< HEAD
-			name="errore";
-
-			json = JSON.stringify({
-				response: "Formatting error"
-			})
-			resp.end(json);
-=======
 			placesNearby(resp,map,citta,1000,"",name,latlngKnown,geocoder);
->>>>>>> develop
 
 			break;
 	}
