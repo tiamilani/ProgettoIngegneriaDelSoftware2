38415b30e3d56744b61a5108a409ee4adbc17eb0
DAS-96 <damiano.sartori@studenti.unitn.it>
2017-11-15 08:50:10 +0100

    correct small errors

--- api/controllers/Controller.js
+++ api/controllers/Controller.js
@@ -1,36 +1,22 @@
 'use strict';
-<<<<<<< HEAD
-
-exports.function = function(req, response) {
-	console.log("Request handler function was called.");
-=======
 const fun = require('../../TelegramBot/functions.js');
 
-
 exports.function = function(req, response) {
-	var par = req.query.ciao;
-	console.log("Request handler list_all_tasks was called.");
->>>>>>> develop
-	response.writeHead(200, {"Content-Type": "application/json"});
-	var otherArray = ["item1", "item2"];
-	var otherObject = { item1: "ciao come stai qui tutt", item2: "item2val" };
-	var json = JSON.stringify({
-	anObject: otherObject,
-	anArray: otherArray,
-<<<<<<< HEAD
-	another: "item"
-=======
-	another: "item",
-	parameterURL: par
->>>>>>> develop
+		var par = req.query.ciao;
+		console.log("Request handler list_all_tasks was called.");
+		response.writeHead(200, {"Content-Type": "application/json"});
+		var otherArray = ["item1", "item2"];
+		var otherObject = { item1: "ciao come stai qui tutt", item2: "item2val" };
+		var json = JSON.stringify({
+		anObject: otherObject,
+		anArray: otherArray,
+		another: "item",
+		parameterURL: par
 	});
 	response.end(json);
 
 	//res.json(task);
 };
-<<<<<<< HEAD
-=======
-
 
 /*---2017-10-31---*/
 exports.dwAvvisi = function(request, response)
@@ -55,4 +41,3 @@ exports.dwAvvisi = function(request, response)
 	}
 	catch (err) { console.log(err.message); }
 };
->>>>>>> develop
