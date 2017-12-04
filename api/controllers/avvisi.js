fa3809e51ae9304931ff771563488bc041b2b784
Brugix <fbrugiolo@gmail.com>
2017-12-04 16:34:13 +0100

    hotfix50 Completata

--- api/controllers/avvisi.js
+++ api/controllers/avvisi.js
@@ -6,13 +6,20 @@ exports.dwAvvisi = function(request, response)
 {
 	var dipartimentoRichiesto = request.query.dipartimento;
 
-	response.writeHead(200, {"Content-Type": "application/json; charset=utf-8"});
-
 	try
 	{
 		alert.richiestaAvvisi(dipartimentoRichiesto, null, null)
 			.then(values => {
-				response.end(values);
+				if(values['urlDipartimento'] === "URL non valido")
+				{
+					response.writeHead(404, {"Content-Type": "application/json; charset=utf-8"});
+					response.end(values);
+				}
+				else
+				{
+					response.writeHead(200, {"Content-Type": "application/json; charset=utf-8"});
+					response.end(values);
+				}
 			})
 			.catch((err) => { console.log(err.message); });
 	}
