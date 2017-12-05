3588d1c076ce8bcee7ed727030832bd71294aca6
iAndrea <fandrea.1996@gmail.com>
2017-12-05 13:39:04 +0100

    Aggiunta API in put per aggiornamento database

--- api/routes/Routes.js
+++ api/routes/Routes.js
@@ -6,46 +6,39 @@ module.exports = function(app) {
 	const urban = require ('../controllers/mezzi.js');
 	const dead = require ('../controllers/scadenze.js');
 	const howto = require ('../controllers/howto.js');
+	const db = require ('../controllers/develop.js');
 	const telegramController = require ('../controllers/telegramController');
 	const TOKEN = process.env.TELEGRAM_TOKEN || '466491462:AAF8RxkhGR00Mylr0LGZfFWUMvPVWSHqUPE';
 
-	// todoList Routes
-	app.route('/example')
-		.get(controller.function)
-		.post(controller.function);
+	app.route('/updatedb')
+		.put(db.UpdateDB);
 
-	app.route("/localizza")
-		.get(place.luoghiUtili);
+	app.route('/example')
+		.get(controller.function);
+
+	app.route("/localizza");
 
 	app.route('/howto')
-		.get(howto.base)
-		.post(howto.base);
+		.get(howto.base);
 
 	app.route('/avvisi')
-		.get(avvisi.dwAvvisi)
-		.post(avvisi.dwAvvisi);
+		.get(avvisi.dwAvvisi);
 
 	app.route('/fermata')
-		.get(urban.Fermata)
-		.post(urban.Fermata);
+		.get(urban.Fermata);
 
 	app.route('/linea')
-		.get(urban.Linea)
-		.post(urban.Linea);
+		.get(urban.Linea);
 
 	app.route('/next')
-		.get(urban.Next)
-		.post(urban.Next);
+		.get(urban.Next);
 
 	app.route('/avvisilinee')
-		.get(urban.Avvisi_Linee)
-		.post(urban.Avvisi_Linee);
+		.get(urban.Avvisi_Linee);
 
 	app.route('/scadenze')
-		.get(dead.mostraScadenze)
-		.post(dead.mostraScadenze);
+		.get(dead.mostraScadenze);
 
-	// We are receiving updates at the route below!
 	app.route(`/bot${TOKEN}`)
 		.post(telegramController.update);
 };
